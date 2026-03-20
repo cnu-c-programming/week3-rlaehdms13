488821367e63cb067bd03536d489e4832a011b75
rlaehdms13 <de060103@gmail.com>
2026-03-20 10:04:28 +0900

    Update 3-3.c

---
+++ code/3-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void average(int count, ...) {
+  va_list args;
+  va_start (args, count);
+  int sum = ;
+  for(int i = 0; i < count; i++){
+    sum += va_arg(args, int);
+  }
+  va_end(args);
+  return sum / count;
+}
+
+int main() {
+  printf("%d\n", average(3, 10, 20, 30));
+  printf("%d\n", average(5, 10, 20, 30, 40, 50));
+  return 0;
+}
