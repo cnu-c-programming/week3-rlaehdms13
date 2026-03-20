dd86f4cb9de4bf3ed6e69491c68aaa1b8c8406a7
rlaehdms13 <de060103@gmail.com>
2026-03-20 10:12:57 +0900

    Update 3-4.c

---
+++ code/3-4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int max_of(count,...) {
+  va_list args;
+  va_start(args, count);
+  int max = 0;
+  for(int i = 0; i < count; i++){
+    int cur = va_arg(args, int);
+    if(cur > max){
+      max = cur;
+    }
+  }
+  va_end(args);
+  return max;
+}
+
+int mian() {
+  printf("%d\n", max_of(4, 5, 12, 3, 9));
+  printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
+  printf("%d\n", max_of(3, 100, 200, 300));
+  return 0;
+}
