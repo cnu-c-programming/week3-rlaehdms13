941b7cd5b47f45c7a8b9354ea28da6166c1cb789
rlaehdms13 <de060103@gmail.com>
2026-03-20 10:15:40 +0900

    Update 3-5.c

---
+++ code/3-5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void
+
+int main() {
+  my_sum('S', 2, "Hello", "World");
+  my_sum('C', 3, "C", "N", "U");
+  my_sum('D', 4, 10, 20, 30, 40);
+
+  return 0;
+}
