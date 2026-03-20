51abf347f170698c635a3a052465d5590084a9c8
rlaehdms13 <de060103@gmail.com>
2026-03-20 09:25:02 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -6,7 +6,7 @@ int mul(int a, int b) { return a * b; }
 int div(int a, int b) { return a / b; }
 
 int main(void) {
-  int a = 10; b = 5;
+  int a = 10, b = 5;
   printf("%d + %d = %d\n", a, b, add(a, b));
   printf("%d - %d = %d\n", a, b, sub(a, b));
   printf("%d * %d = %d\n", a, b, mul(a, b));
