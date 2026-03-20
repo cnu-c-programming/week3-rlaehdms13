27ba050abd0dd7de44bcf2b1014a2acba70a6bc7
rlaehdms13 <de060103@gmail.com>
2026-03-20 09:30:40 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print();
+
+int main() {
+  print();
+
+  int num = 5;
+  int result = square(num);
+  printf("%d %d\n", num, result);
+
+  return 0;
+}
+
+void print() {
+  printf("Welcome to Computer Programming 03 Lab!\n");
+}
+
+int square(int n) {
+  return n * n;
+}
