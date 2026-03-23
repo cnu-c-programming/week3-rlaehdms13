dfc8c594aa0d1cda76fa146286a59903bec2d55b
rlaehdms13 <de060103@gmail.com>
2026-03-23 10:16:20 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -10,7 +10,7 @@ void test_scope() {
   static_var++;
   local_var++;
   printf("global_var: %d\n", global_var);
-  printf("staic_var: %d\n", static_var);
+  printf("static_var: %d\n", static_var);
   printf("local_var: %d\n", local_var);
 }
 
